115011aa156e07deb3c0ea0d3b441d8c3e2963f2
Jose Luis Ferrete <joseluisferrete@gmail.com>
2018-10-07 19:12:33 +0200

    Relations between classes

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    // $category -> products
+
+    public function products()
+    {
+    	return $this->hasMany(Product::class);
+    }
 }
--- app/ProductImage.php
+++ app/ProductImage.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductImage extends Model
 {
-    //
+    //$productImage->product
+
+    public function product()
+    {
+    	return $this->belongsTo(Product::class);
+    }
 }
